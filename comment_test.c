dba8d74ab1539fd0e5c2acb68b4506aae22cca7e
vignesh. R <120434547+vigneshvicky0246@users.noreply.github.com>
2024-03-19 12:56:15 +0530

    Add files via upload

---
+++ comment_test.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main()
+{
+int a,b,c; 
+/*varible declaration*/ 
+printf(“enter two numbers”);
+ scanf(“%d %d”,&a,&b);
+  c=a+b;
+  //adding two numbers printf(“sum is %d”,c);
+return 0;
+}
